76d0337e2d987604a4d2354bdf76366d5069ea97
Aleksey Serebryakov <vinni-pux@yandex.ru>
2023-11-02 16:13:33 +0300

    отформатирована дата

--- src/main/java/main/dto/MessageMapper.java
+++ src/main/java/main/dto/MessageMapper.java
@@ -2,10 +2,17 @@ package main.dto;
 
 import main.model.Message;
 
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.logging.SimpleFormatter;
+
 public class MessageMapper {
     public static DtoMessage map(Message message) {
         DtoMessage dtoMessage = new DtoMessage();
-        dtoMessage.setDatetime(message.getDateTime().toString());
+        dtoMessage.setDatetime(message.getDateTime()
+                .format(DateTimeFormatter
+                        .ofLocalizedDateTime(FormatStyle.MEDIUM)));
         dtoMessage.setUsername(message.getUser().getName());
         dtoMessage.setText(message.getMessage());
         return dtoMessage;
